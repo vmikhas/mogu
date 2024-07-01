2738ac52ced12b533d83df0b465a404fdf15bb26
developer <developer@developer.ru>
2024-07-01 18:11:32 +0300

    сделал блок order

--- src/order/Order.js
+++ src/order/Order.js
@@ -1,3 +1,5 @@
+import folder from "./../assest/images/folder.png";
+
 export default function Order() {
 
   return (
@@ -6,7 +8,7 @@ export default function Order() {
         <div className="order__wrapper">
           <h2 className="order__title">Порядок в подарок</h2>
           <p className="order__desc"> задачи удобно структурировать с помощью чек-листов, меток и пользовательских полей</p>
-
+          <img className="order__image" src={folder} alt="" width={400} height={300}/>
           <ul className="order__list">
             <li className="order__item">обновляйте статус задачи </li>
             <li className="order__item">следите за сроками дедлайнов</li>
