e1582b8c9672b715777e41cbaf467b3368b734bc
developer <developer@developer.ru>
2024-07-01 17:06:50 +0300

    добавил миксин свойств для адаптивности

--- src/order/Order.js
+++ src/order/Order.js
@@ -1,19 +1,19 @@
 export default function Order() {
 
-    return (
-        <section className="order">
-            <div className="order__container container">
-                <div className="order__wrapper">
-                    <h2 className="order__title">Порядок в подарок</h2>
-                    <p className="order__desc"> задачи удобно структурировать с помощью чек-листов, меток и пользовательских полей</p>
+  return (
+    <section className="order">
+      <div className="order__container container">
+        <div className="order__wrapper">
+          <h2 className="order__title">Порядок в подарок</h2>
+          <p className="order__desc"> задачи удобно структурировать с помощью чек-листов, меток и пользовательских полей</p>
 
-                    <ul className="order__list">
-                        <li className="order__item">обновляйте статус задачи </li>
-                        <li className="order__item">следите за сроками дедлайнов</li>
-                        <li className="order__item">отправляйте завершенные задачи в архив</li>
-                    </ul>
-                </div>
-            </div>
-        </section>
-    );
+          <ul className="order__list">
+            <li className="order__item">обновляйте статус задачи </li>
+            <li className="order__item">следите за сроками дедлайнов</li>
+            <li className="order__item">отправляйте завершенные задачи в архив</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/promo/Promo.js
+++ src/promo/Promo.js
@@ -4,23 +4,23 @@ import image_g from "./../assest/images/image_g.png";
 import image_u from "./../assest/images/image_u.png";
 
 export default function Promo () {
-    const images = [
-		image_m,
-		image_o,
-		image_g,
-		image_u
-	];
+  const images = [
+  	image_m,
+  	image_o,
+  	image_g,
+  	image_u
+  ];
 
     // const a = 1;
     // const b = `item-${a}`;
 
-    return (
-        <section className="promo">
-            <div className="promo__container">
-                <ul className="promo__list">
-					{images.map((image,id) => <li className={`promo__item promo__item_${id}`} key={'item-'+id}><img className="promo__image" src={image} alt=""/></li>)}
-				</ul>
-            </div>
-        </section>
-    );
+  return (
+    <section className="promo">
+      <div className="promo__container">
+        <ul className="promo__list">
+	      {images.map((image,id) => <li className={`promo__item promo__item--${id}`} key={'item-'+id}><img className="promo__image" src={image} alt=""/></li>)}
+	    </ul>
+      </div>
+    </section>
+  );
 }
